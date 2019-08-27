678c5229b6550a79467372dd5f8d252c63b60fb6
zhouqiii <860178804@qq.com>
2019-08-27 16:57:47 +0800

    sample

--- 作用域和闭包/执行上下文.js
+++ 作用域和闭包/执行上下文.js
@@ -25,10 +25,10 @@
     }
 //执行上下文：<script></script>标签或者函数会形成一个执行上下文，在script执行上下文里，首先会进行变量定义和函数声明，然后这里的函数在执行之前也会先进行变量定义和函数声明，以及this、arguments声明
 //函数声明和函数表达式的区别：fn('zhangsan')//zhangsan-函数声明
-                            function fn(name){
-                                console.log(name)
-                            }
-                            fn('zhangsan')//会报错-函数表达式
-                            var fn1=function(name){//fn1只是一个变量，是变量的用法
-                                console.log(name)
-                            }
+        function fn(name){
+            console.log(name)
+        }
+        fn('zhangsan')//会报错-函数表达式
+        var fn1=function(name){//fn1只是一个变量，是变量的用法
+            console.log(name)
+        }
